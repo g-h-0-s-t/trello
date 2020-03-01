e0009bc3b3a92ee679cb0a1508c1ce96332f664c
Sol <discounttechwarehouse@gmail.com>
2020-03-01 10:47:51 +0000

    express api webhook test

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const trello = require('./trello.js')
+const app = express()
+const port = 3000
+
+const API_KEY = 'eece05e3fbcf1e761c982cbcc3148bf7';
+const TOKEN = 'cbd97c1319aa90c3c254418de55384afb3220187d54b12d91b7dd74c1be45fc4';
+const AUTH = `key=${API_KEY}&token=${TOKEN}`
+
+// !req.params.id ? res.send('Invalid Params.', 404) : trello(req.params.id)
+app.post('/api', (req, res) => console.log(req))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
