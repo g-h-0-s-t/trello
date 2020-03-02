d6b2e3ef7dca9f529590dd8f9f025703cb4b8f65
Sol <discounttechwarehouse@gmail.com>
2020-03-02 19:37:17 +0000

    updating

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const trello = require('./trello.js')
-const app = express()
+const trello = require('./trello.js');
+const app = express();
 const port = process.env.PORT || 8080
 const API_KEY = 'eece05e3fbcf1e761c982cbcc3148bf7';
 const TOKEN = 'cbd97c1319aa90c3c254418de55384afb3220187d54b12d91b7dd74c1be45fc4';
-const AUTH = `key=${API_KEY}&token=${TOKEN}`
+const AUTH = `key=${API_KEY}&token=${TOKEN}`;
 
 // Finds all cards and updates them
-trello.start(AUTH)
+// trello.start(AUTH)
 
 // Head request shows trello callback is valid
 app.use(express.json())
@@ -15,7 +15,7 @@ app.use(express.json())
 app.head('/api/notifications/boards', (req, res) => res.status(200))
 app.post('/api/notifications/boards', (req, res) => req?.action?.createBoard ? trello.createHook(TOKEN, URL, req.data.board.id) : '')
 // Watch boards for new cards
-app.head('/api/notifications/cards', res => res.status(200))
+app.head('/api/notifications/cards', (req, res) => res.status(200))
 app.post('/api/notifications/cards', (req, res) => !req?.action?.createCard ? trello.updateCard(AUTH, req.data.card) : '')
 
 app.listen(port)
