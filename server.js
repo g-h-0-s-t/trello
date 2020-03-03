5b733b99846f7732ddd68022e0e4056cb9943551
Sol <discounttechwarehouse@gmail.com>
2020-03-03 11:04:23 +0000

    a

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ const TOKEN = 'cbd97c1319aa90c3c254418de55384afb3220187d54b12d91b7dd74c1be45fc4'
 const AUTH = `key=${API_KEY}&token=${TOKEN}`;
 
 // Finds all cards and updates them
-trello.start(AUTH)
+app.get('/api/start', req, res => res.status(201).json({value: trello.start(AUTH)})
 
 // Head request shows trello callback is valid
 app.use(express.json())
 // Watch member for new board
 app.head('/api/notifications/boards', (req, res) => res.status(200).json({value: 'success'}))
 app.post('/api/notifications/boards', (req, res) => { 
-    req.action !== undefined ? req.action.createBoard ? trello.createHook(TOKEN, URL, req.data.board.id) : res.json({'value': null}) : res.json({'value': null})})
+    req.action !== undefined ? req.action.createBoard ? trello.createHook(TOKEN, URL, req.data.board.id) : res.json({'value': null}) : res.json({'value': null})
+
+})
 // Watch boards for new cards
 app.head('/api/notifications/cards', (req, res) => res.status(200).json({value: 'success'}))
 app.post('/api/notifications/cards', (req, res) => !req.action.createCard ? trello.updateCard(AUTH, req.data.card) : res.json({'value': null}))
