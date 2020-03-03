abcc2b32721d4833ec474b0896da2c933e5de555
Sol <discounttechwarehouse@gmail.com>
2020-03-03 13:59:13 +0000

    a

--- trello.js
+++ trello.js
@@ -50,7 +50,7 @@ function createHook(TOKEN, URL, watch) {
     };
 
     axios.post(`https://api.trello.com/1/tokens/${TOKEN}/webhooks`, data)
-        .then(res => res.ok ? console.log(res.data : console.log(false))
+        .then(res => res.ok ? console.log(res.data) : console.log(false))
          .catch(err => handleErrors(err))
     
     return watch;
