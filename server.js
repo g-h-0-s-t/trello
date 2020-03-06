cdf9ecd2716f02857cd202e6beb69976ac8ed445
gh0st <discounttechwarehouse@gmail.com>
2020-03-06 10:25:10 +0000

    updated token

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const trello = require('./trello.js');
 const app = express();
 const port = process.env.PORT || 8080
 const API_KEY = 'eece05e3fbcf1e761c982cbcc3148bf7';
-const TOKEN = 'cbd97c1319aa90c3c254418de55384afb3220187d54b12d91b7dd74c1be45fc'
+const TOKEN = 'cbd97c1319aa90c3c254418de55384afb3220187d54b12d91b7dd74c1be45fc4'
 const AUTH = `key=${API_KEY}&token=${TOKEN}`;
 // 
 // Finds all cards and updates them
