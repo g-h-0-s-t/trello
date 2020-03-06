5bc07c95a8ac33c2e7abcad017341b6c9331a11a
gh0st <discounttechwarehouse@gmail.com>
2020-03-06 09:53:00 +0000

    new token

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080
 const API_KEY = 'eece05e3fbcf1e761c982cbcc3148bf7';
 const TOKEN = 'cbd97c1319aa90c3c254418de55384afb3220187d54b12d91b7dd74c1be45fc'
 const AUTH = `key=${API_KEY}&token=${TOKEN}`;
-
+// 
 // Finds all cards and updates them
 app.get('/api/start', (req, res) => res.status(201).json({value: trello.start(AUTH)}))
 
